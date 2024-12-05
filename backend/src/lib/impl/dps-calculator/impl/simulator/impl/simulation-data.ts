9c533ee24c02f996482fdbd3922d478d2590a560
Eltik <xp10d363@gmail.com>
2024-12-05 12:31:21 -0800

    feat: Reset DPS calculator

--- backend/src/lib/impl/dps-calculator/impl/simulator/impl/simulation-data.ts
+++
@@ -1,9 +0,0 @@
-export default class SimulationData {
-    public operatorDamageTotalData: number[] = [];
-    public operatorDPSTotalData: number[] = [];
-
-    public enemyDefData: number = 0;
-    public enemyResData: number = 0;
-
-    public seriesName: string = "";
-}
