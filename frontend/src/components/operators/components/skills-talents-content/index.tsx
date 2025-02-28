799ff5b84beb6c60855e57ec54d2805c1d3f5dfe
Eltik <xp10d363@gmail.com>
2025-02-27 23:40:01 -0800

    feat: Talents content

--- frontend/src/components/operators/components/skills-talents-content/index.tsx
+++ frontend/src/components/operators/components/skills-talents-content/index.tsx
@@ -1,20 +1,9 @@
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Separator } from "~/components/ui/separator";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import type { Operator } from "~/types/impl/api/static/operator";
-import SkillCard from "./impl/skill-card";
-import { useState } from "react";
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/components/ui/collapsible";
-import { ChevronDown } from "lucide-react";
-import { AnimatePresence, motion } from "framer-motion";
-import { Slider } from "~/components/ui/slider";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
-import Image from "next/image";
+import SkillsContent from "./impl/skills-content";
+import TalentsContent from "./impl/talents-content";
 
-function SkillsContent({ operator }: { operator: Operator }) {
-    const [skillLevel, setSkillLevel] = useState(9);
-    const [isSkillsOpen, setIsSkillsOpen] = useState(true);
-
+function SkillsTalentsContent({ operator }: { operator: Operator }) {
     return (
         <>
             <div>
@@ -24,87 +13,8 @@ function SkillsContent({ operator }: { operator: Operator }) {
                 <Separator />
                 <div className="grid px-3 md:p-4">
                     <div className="space-y-8 md:col-span-2">
-                        <Collapsible defaultOpen={true} open={isSkillsOpen} onOpenChange={setIsSkillsOpen}>
-                            <Card>
-                                <CollapsibleTrigger className="w-full cursor-pointer" asChild>
-                                    <CardHeader className="flex w-full flex-row items-center justify-between space-y-0 p-4">
-                                        <CardTitle>Skills</CardTitle>
-                                        <ChevronDown className={`ml-auto h-6 w-6 transition-transform ${isSkillsOpen ? "rotate-180" : ""}`} />
-                                    </CardHeader>
-                                </CollapsibleTrigger>
-                                <AnimatePresence initial={false}>
-                                    {isSkillsOpen && (
-                                        <motion.div
-                                            key="content"
-                                            initial="collapsed"
-                                            animate="open"
-                                            exit="collapsed"
-                                            variants={{
-                                                open: { opacity: 1, height: "auto" },
-                                                collapsed: { opacity: 0, height: 0 },
-                                            }}
-                                            transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
-                                        >
-                                            <CollapsibleContent forceMount>
-                                                <CardContent>
-                                                    <Tabs defaultValue={operator.skills[operator.skills.length - 1]?.skillId} className="w-full">
-                                                        <TabsList className={`grid w-full grid-cols-${operator.skills.length}`}>
-                                                            {operator.skills.map((skill) => (
-                                                                <TabsTrigger value={skill.skillId} key={skill.skillId}>
-                                                                    {skill.static?.levels[0]?.name}
-                                                                </TabsTrigger>
-                                                            ))}
-                                                        </TabsList>
-                                                        <div className="mb-6 mt-4 flex flex-col">
-                                                            <div className="flex flex-col">
-                                                                <span className="text-lg font-bold">Skill Level</span>
-                                                                <span className="text-sm text-muted-foreground">Drag the slider to change the skill level</span>
-                                                            </div>
-                                                            <div className="flex max-w-[80%] flex-col items-center gap-2 md:flex-row">
-                                                                <Slider className="w-full" defaultValue={[skillLevel]} value={[skillLevel]} onValueChange={(value) => setSkillLevel(value[0] ?? 0)} min={0} max={(operator.skills[0]?.static?.levels ?? []).length - 1} step={1} />
-                                                                <div className="flex w-full flex-row">
-                                                                    <Select value={skillLevel.toString()} onValueChange={(e) => setSkillLevel(parseInt(e))}>
-                                                                        <SelectTrigger className="w-[180px]">
-                                                                            <SelectValue placeholder={skillLevel} />
-                                                                        </SelectTrigger>
-                                                                        <SelectContent>
-                                                                            {(operator.skills[0]?.static?.levels ?? []).map((_, i) => (
-                                                                                <SelectItem key={i} value={i.toString()}>
-                                                                                    {i < 7 ? <span>Lv.{i + 1}</span> : <span>M{i === 7 ? "1" : i === 8 ? "2" : "3"}</span>}
-                                                                                </SelectItem>
-                                                                            ))}
-                                                                        </SelectContent>
-                                                                    </Select>
-                                                                    {skillLevel >= 7 && (
-                                                                        <Image
-                                                                            src={`https://ak.gamepress.gg/sites/default/files/2019-10/m-${skillLevel === 7 ? "1" : skillLevel === 8 ? "2" : "3"}_0.png`}
-                                                                            className="h-8 w-9"
-                                                                            width={50}
-                                                                            height={50}
-                                                                            alt="M1"
-                                                                            style={{
-                                                                                maxWidth: "100%",
-                                                                                height: "auto",
-                                                                            }}
-                                                                        />
-                                                                    )}
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                        {operator.skills.map((skill) => (
-                                                            <TabsContent value={skill.skillId} key={skill.skillId}>
-                                                                <SkillCard skill={skill} level={skillLevel} />
-                                                                {/* Damage Calculation */}
-                                                            </TabsContent>
-                                                        ))}
-                                                    </Tabs>
-                                                </CardContent>
-                                            </CollapsibleContent>
-                                        </motion.div>
-                                    )}
-                                </AnimatePresence>
-                            </Card>
-                        </Collapsible>
+                        <SkillsContent operator={operator} />
+                        <TalentsContent operator={operator} />
                     </div>
                 </div>
             </div>
@@ -112,4 +22,4 @@ function SkillsContent({ operator }: { operator: Operator }) {
     );
 }
 
-export default SkillsContent;
+export default SkillsTalentsContent;
--- frontend/src/pages/api/static.ts
+++ frontend/src/pages/api/static.ts
@@ -1,5 +1,6 @@
 import { type ServerResponse } from "http";
 import type Module from "module";
+import { unstable_cache } from "next/cache";
 import { env } from "~/env.js";
 import type { Item } from "~/types/impl/api/static/material";
 import type { ModuleData, Modules } from "~/types/impl/api/static/modules";
@@ -7,185 +8,184 @@ import type { Operator } from "~/types/impl/api/static/operator";
 import type { Ranges } from "~/types/impl/api/static/ranges";
 import type { Skill } from "~/types/impl/api/static/skills";
 
+// Cache configuration
+const CACHE_TAG = "static-api";
+const CACHE_TTL = 3600; // Cache lifetime in seconds (1 hour)
+
+// Function to fetch data from backend with caching
+const fetchWithCache = unstable_cache(
+    async <T>(endpoint: string, body: object, tags: string[] = []): Promise<T> => {
+        const response = await fetch(`${env.BACKEND_URL}${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Backend request failed with status ${response.status}`);
+        }
+
+        return response.json() as Promise<T>;
+    },
+    // Cache key generation function as string[]
+    ["static-api-cache"],
+    // Cache options
+    {
+        tags: [CACHE_TAG],
+        revalidate: CACHE_TTL,
+    },
+);
+
 export default async function handler(request: Request, response: ServerResponse) {
-    switch (request.body.type) {
-        case "materials":
-            const materials = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
-                    }),
-                })
-            ).json()) as {
-                materials: Item[];
-            };
+    try {
+        switch (request.body.type) {
+            case "materials": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: materials.materials,
-                }),
-            );
-            return response.end();
-        case "modules":
-            const modules = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
-                        method: request.body.method,
-                    }),
-                })
-            ).json()) as
-                | {
-                      modules: Modules;
-                  }
-                | {
-                      details: ModuleData;
-                  }
-                | {
-                      modules: Module[];
-                  }
-                | {
-                      modules: Module;
-                  };
+                const materials = await fetchWithCache<{ materials: Item[] }>("/static", requestBody, [`${CACHE_TAG}-materials-${request.body.id ?? "all"}`]);
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(JSON.stringify(modules));
-            return response.end();
-        case "operators":
-            const operators = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: materials.materials,
                     }),
-                })
-            ).json()) as {
-                operators: Operator[];
-            };
+                );
+                return response.end();
+            }
+            case "modules": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                    method: request.body.method,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: operators.operators,
-                }),
-            );
-            return response.end();
-        case "ranges":
-            const ranges = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
+                const modules = await fetchWithCache<{ modules: Modules } | { details: ModuleData } | { modules: Module[] } | { modules: Module }>("/static", requestBody, [`${CACHE_TAG}-modules-${request.body.id ?? "all"}-${request.body.method ?? "default"}`]);
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(JSON.stringify(modules));
+                return response.end();
+            }
+            case "operators": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                };
+
+                const operators = await fetchWithCache<{ operators: Operator[] }>("/static", requestBody, [`${CACHE_TAG}-operators-${request.body.id ?? "all"}`]);
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: operators.operators,
                     }),
-                })
-            ).json()) as {
-                range: Ranges;
-            };
+                );
+                return response.end();
+            }
+            case "ranges": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: ranges.range,
-                }),
-            );
-            return response.end();
-        case "skills":
-            const skills = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
+                const ranges = await fetchWithCache<{ range: Ranges }>("/static", requestBody, [`${CACHE_TAG}-ranges-${request.body.id ?? "all"}`]);
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: ranges.range,
                     }),
-                })
-            ).json()) as {
-                skills: Skill[];
-            };
+                );
+                return response.end();
+            }
+            case "skills": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: skills.skills,
-                }),
-            );
-            return response.end();
-        case "trust":
-            const trust = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
-                        trust: request.body.trust,
+                const skills = await fetchWithCache<{ skills: Skill[] }>("/static", requestBody, [`${CACHE_TAG}-skills-${request.body.id ?? "all"}`]);
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: skills.skills,
                     }),
-                })
-            ).json()) as {
-                trust: number | null;
-            };
+                );
+                return response.end();
+            }
+            case "trust": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                    trust: request.body.trust,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: trust.trust,
-                }),
-            );
-            return response.end();
-        case "handbook":
-            const handbook = (await (
-                await fetch(`${env.BACKEND_URL}/static`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        type: request.body.type,
-                        id: request.body.id,
+                // Trust data can be modified, so we need to be careful with caching
+                // Only cache GET requests (without trust parameter)
+                const useCache = request.body.trust === undefined;
+
+                let trust;
+                if (useCache) {
+                    trust = await fetchWithCache<{ trust: number | null }>("/static", requestBody, [`${CACHE_TAG}-trust-${request.body.id ?? "all"}`]);
+                } else {
+                    // For updates, bypass cache
+                    const response = await fetch(`${env.BACKEND_URL}/static`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(requestBody),
+                    });
+                    trust = (await response.json()) as { trust: number | null };
+                }
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: trust.trust,
                     }),
-                })
-            ).json()) as {
-                handbook: string;
-            };
+                );
+                return response.end();
+            }
+            case "handbook": {
+                const requestBody = {
+                    type: request.body.type,
+                    id: request.body.id,
+                };
 
-            response.writeHead(200, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    data: handbook.handbook,
-                }),
-            );
-            return response.end();
-        default:
-            response.writeHead(400, { "Content-Type": "application/json" });
-            response.write(
-                JSON.stringify({
-                    error: "Invalid type.",
-                }),
-            );
-            return response.end();
+                const handbook = await fetchWithCache<{ handbook: string }>("/static", requestBody, [`${CACHE_TAG}-handbook-${request.body.id ?? "all"}`]);
+
+                response.writeHead(200, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        data: handbook.handbook,
+                    }),
+                );
+                return response.end();
+            }
+            default:
+                response.writeHead(400, { "Content-Type": "application/json" });
+                response.write(
+                    JSON.stringify({
+                        error: "Invalid type.",
+                    }),
+                );
+                return response.end();
+        }
+    } catch (error) {
+        console.error("API error:", error);
+        response.writeHead(500, { "Content-Type": "application/json" });
+        response.write(
+            JSON.stringify({
+                error: "An error occurred while processing the request.",
+            }),
+        );
+        return response.end();
     }
 }
 
