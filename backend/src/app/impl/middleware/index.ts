9156a4516d48998d5bf5a139b82707012dbc59e3
Eltik <xp10d363@gmail.com>
2025-04-11 09:24:18 -0700

    feat: CDN routes

--- backend/src/app/impl/middleware/index.ts
+++ backend/src/app/impl/middleware/index.ts
@@ -1,7 +1,9 @@
 import rateLimit from "./impl/rateLimit";
 import createResponse from "./impl/response";
+import serveAsset from "./impl/cdn";
 
 export default {
     rateLimit,
     createResponse,
+    serveAsset,
 };
