257b336fe0ec37023b991f1ab8c9857723e8a234
Eltik <xp10d363@gmail.com>
2025-12-08 10:54:16 -0800

    feat: Use layout

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppType } from "next/app";
 import { Geist } from "next/font/google";
+import { Layout } from "~/components/layout/layout";
 
 import "~/styles/globals.css";
 
@@ -10,7 +11,9 @@ const geist = Geist({
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
         <div className={geist.className}>
-            <Component {...pageProps} />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </div>
     );
 };
