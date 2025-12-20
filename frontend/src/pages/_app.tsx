eed8d1b5542c6e2244125befc9278dbaee0f7c57
Eltik <xp10d363@gmail.com>
2025-12-20 15:40:02 -0700

    feat: Start of login dialogue

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppType } from "next/app";
 import { Geist } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import { PagesTopLoader } from "nextjs-toploader/pages";
 import { Layout } from "~/components/layout/layout";
+import { Toaster } from "~/components/ui/shadcn/sonner";
 
 import "~/styles/globals.css";
 
@@ -11,12 +13,15 @@ const geist = Geist({
 
 const MyApp: AppType = ({ Component, pageProps }) => {
     return (
-        <div className={geist.className}>
-            <PagesTopLoader color="#e66e68" height={4} showSpinner={false} />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </div>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            <div className={geist.className}>
+                <PagesTopLoader color="#e66e68" height={4} showSpinner={false} />
+                <Layout>
+                    <Toaster />
+                    <Component {...pageProps} />
+                </Layout>
+            </div>
+        </ThemeProvider>
     );
 };
 
