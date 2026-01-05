dfcd34dee0d5551563ae08bd9c8d4f96408b859d
Eltik <xp10d363@gmail.com>
2026-01-05 14:00:59 -0800

    feat: Remove rest of home page, keep hero

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { CTASection } from "~/components/home/cta";
-import { FeaturesSection } from "~/components/home/features";
 import { HeroSection } from "~/components/home/hero";
 
 export default function HomePage() {
     return (
         <div className="relative w-full">
             <HeroSection />
-            <FeaturesSection />
-            <CTASection />
         </div>
     );
 }
