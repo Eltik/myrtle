4cb15395485f4d1699b58fc4eea8f74b0a0d1695
Eltik <xp10d363@gmail.com>
2026-01-12 09:57:41 -0800

    feat: Search page v1

--- frontend/src/components/ui/shadcn/collapsible.tsx
+++ frontend/src/components/ui/shadcn/collapsible.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import { cn } from "~/lib/utils";
 
 function Collapsible({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
     return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />;
@@ -10,8 +11,8 @@ function CollapsibleTrigger({ ...props }: React.ComponentProps<typeof Collapsibl
     return <CollapsiblePrimitive.CollapsibleTrigger data-slot="collapsible-trigger" {...props} />;
 }
 
-function CollapsibleContent({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
-    return <CollapsiblePrimitive.CollapsibleContent data-slot="collapsible-content" {...props} />;
+function CollapsibleContent({ className, ...props }: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+    return <CollapsiblePrimitive.CollapsibleContent className={cn("overflow-hidden transition-all data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down", className)} data-slot="collapsible-content" {...props} />;
 }
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent };
