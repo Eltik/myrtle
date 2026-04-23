1113a9155db5f533f83d4e7e93624abd36b73467
Ian Lew <eltik@Ians-MacBook-Pro.local>
2026-04-23 14:58:16 -0700

    chore: Explicit pathing for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = defineConfig({
-    resolve: { tsconfigPaths: true },
+    resolve: {
+        tsconfigPaths: true,
+        alias: [
+            {
+                find: /^@base-ui\/utils\/(.+)$/,
+                replacement: path.resolve(__dirname, "node_modules/@base-ui/utils/esm") + "/$1.js",
+            },
+        ],
+    },
     plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 });
 
