b33e5a1cfb73d4940e58398ff95f99ca2768f3a0
Ian Lew <eltik@Ians-MacBook-Pro.local>
2026-04-23 11:39:11 -0700

    feat: Icons

--- frontend/src/components/operators/impl/components/ClassIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { env } from "#/env";
-import { formatProfession } from "#/lib/utils";
-
-export function ClassIcon({ profession, size = 20 }: { profession: string; size?: number }) {
-    const base = env.VITE_BACKEND_URL ?? "";
-    const src = `${base}/api/assets/textures/arts/ui/%5Buc%5Dcharcommon/icon_profession_${profession.toLowerCase()}.png`;
-    return <img alt={formatProfession(profession)} src={src} width={size} height={size} />;
-}
