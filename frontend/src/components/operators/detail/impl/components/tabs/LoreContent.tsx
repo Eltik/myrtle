2d52c78df90c9be9733cad76aba99b37f59c5f73
Ian Lew <eltik@Ians-MacBook-Pro.local>
2026-04-26 14:05:03 -0700

    feat: Operator tabs

--- frontend/src/components/operators/detail/impl/components/tabs/LoreContent.tsx
+++ frontend/src/components/operators/detail/impl/components/tabs/LoreContent.tsx
@@ -1,10 +1,10 @@
-import type { IOperatorListItem } from "#/types/operators";
 import { memo } from "react";
+import type { IOperatorListItem } from "#/types/operators";
 
 interface ILoreContentProps {
     operator: IOperatorListItem;
 }
 
 export const LoreContent = memo(function LoreContent({ operator }: ILoreContentProps) {
-    return <></>
+    return <></>;
 });
